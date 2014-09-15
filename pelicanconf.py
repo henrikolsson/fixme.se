917d73fef876e2685377ce93f58676ee34d1a5c9
Henrik Olsson <henrik@fixme.se>
2014-09-15 21:28:53 +0200

    Updated theme for compatibility with newer pelican and a different look

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,11 @@
 
 AUTHOR = u'Henrik Olsson'
 SITENAME = u'fixme.se'
-SITEURL = ''
+
+#SITEURL="http://localhost:8000"
+SITEURL="https://fixme.se"
+
+RELATIVE_URLS = False
 
 TIMEZONE = 'Europe/Stockholm'
 
@@ -25,13 +29,12 @@ ARTICLE_URL = '/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 ARTICLE_LANG_URL = '/{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}.html'
 PAGE_URL = '{date:%Y}/{date:%m}/{date:%d}/pages/{slug}.html'
 PAGE_LANG_URL = '{date:%Y}/{date:%m}/{date:%d}/pages/{slug}-{lang}.html'
-ARTICLE_SAVE_AS = '/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
-ARTICLE_LANG_SAVE_AS = '/{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}.html'
-PAGE_SAVE_AS = '/{date:%Y}/{date:%m}/{date:%d}/pages/{slug}.html'
-PAGE_LANG_SAVE_AS = '/{date:%Y}/{date:%m}/{date:%d}/pages/{slug}-{lang}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_LANG_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}.html'
+PAGE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/pages/{slug}.html'
+PAGE_LANG_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/pages/{slug}-{lang}.html'
 
 THEME="themes/default"
 
-SITEURL="http://fixme.se"
+MD_EXTENSIONS = ['codehilite(css_class=highlight,guess_lang=False)']
 
-RELATIVE_URLS = False
